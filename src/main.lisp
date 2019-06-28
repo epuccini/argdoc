1017d77752d70b5c66f64eca6d871e87b158c0c2
Edward Puccini <edward@Edwards-MacBook-Air.local>
2019-06-28 16:37:54 +0200

    Initial commit

---
+++ src/main.lisp
@@ -0,0 +1,9 @@
+;;; -----------------------------------------------------
+;;; argdoc - application main file
+;;; -----------------------------------------------------
+;;; File:     /Users/edward/Documents/Code/common-lisp/projects/argdoc/src/main.lisp
+;;; Date:     16:33:43 of Friday, 6/28/2019 (GMT+1)
+;;; Author:   Edward Puccini
+;;; -----------------------------------------------------
+
+(in-package :argdoc)
---
+++ src/package.lisp
@@ -0,0 +1,13 @@
+;;; -----------------------------------------------------
+;;; argdoc - package file
+;;; -----------------------------------------------------
+;;; File:     /Users/edward/Documents/Code/common-lisp/projects/argdoc/src/package.lisp
+;;; Date:     16:33:43 of Friday, 6/28/2019 (GMT+1)
+;;; Author:   Edward Puccini
+;;; -----------------------------------------------------
+
+(defpackage :argdoc
+    (:use "COMMON-LISP")
+    (:export
+))
+
